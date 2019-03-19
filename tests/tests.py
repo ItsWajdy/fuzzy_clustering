ac207bd095df1a690524c42b0a7ff53cd0e70079
Wajdy <wajdy1011000@gmail.com>
2019-03-20 00:08:01 +0200

    Implement functionality to initialize a c-means model with all of its required parameters

--- tests/tests.py
+++ tests/tests.py
@@ -1,3 +1,13 @@
 from fuzzy_clustering.c_means import Model
+import numpy as np
+
 
 model = Model()
+model.fit(np.eye(3), 2, 2, 1)
+
+print(model.A)
+print(model.V)
+print(model.U)
+
+# arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# print(np.min(arr[0]))
