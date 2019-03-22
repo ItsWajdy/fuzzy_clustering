e8a78fb9e7628b37009521f43ccd983d61114c2e
Wajdy <wajdy1011000@gmail.com>
2019-03-22 17:27:43 +0200

    Add tests for clustering algorithm

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,7 @@
 from fuzzy_clustering.c_means import Model
 import numpy as np
+import matplotlib.pyplot as plt
+import random
 
 
 model = Model()
@@ -16,4 +18,27 @@ Z = np.array([
 	[9, 2]
 ]).transpose()
 
-model.fit(Z, 2, 2, 1)
+model.fit(Z, 2, 2, 0.01)
+
+plt.scatter(Z[0, :], Z[1, :])
+plt.scatter(model.V[0, :], model.V[1, :], marker='x', c='r')
+plt.show()
+
+points = []
+for i in range(50):
+	x = random.uniform(0, 4)
+	y = random.uniform(6, 10)
+	points.append([x, y])
+
+for i in range(50):
+	x = random.uniform(6, 10)
+	y = random.uniform(0, 4)
+	points.append([x, y])
+
+Z = np.array(points).transpose()
+model = Model()
+model.fit(Z, 2, 2, 0.01)
+
+plt.scatter(Z[0, :], Z[1, :])
+plt.scatter(model.V[0, :], model.V[1, :], marker='x', c='r')
+plt.show()
